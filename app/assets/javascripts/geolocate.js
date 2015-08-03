4e67531d16fd312956f5b649369fd803e8e3b593
walterbm <walter.beller.morales@gmail.com>
2015-08-03 16:39:36 -0400

    add js geolocation

--- app/assets/javascripts/geolocate.js
+++ app/assets/javascripts/geolocate.js
@@ -1,14 +1,11 @@
-function getCoords(){
-if (navigator.geolocation) {
-  navigator.geolocation.getCurrentPosition(function(position){
-    var coords = [];
-    var latitude = position.coords.latitude;
-    var longitude = position.coords.longitude;
-    coords.push(latitude);
-    coords.push(longitude);
-    alert(coords);
-  });
-} else {
-  document.write('Your browser does not support GeoLocation');
-}
+function getCoords(callback){
+  var latitude, longitude;
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(function(position){
+      callback(position.coords.latitude, position.coords.longitude);
+    });
+  } 
+  else {
+    alert('Your browser does not support GeoLocation');
+  }
 }
