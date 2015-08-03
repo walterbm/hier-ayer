7ae3a81092d41eaf9cb87e5c32d64a53e303a2c3
ryannyunn <ryannyunn@gmail.com>
2015-08-03 17:14:15 -0400

    map create new now works

--- app/controllers/maps_controller.rb
+++ app/controllers/maps_controller.rb
@@ -4,9 +4,12 @@ class MapsController < ApplicationController
   end
   
   def create
-    
-    binding.pry
-    
+    @map = Map.create(map_params)
+    @map.user = current_user
+    @moment = Moment.create(moment_params)
+    @map.moments << @moment
+    @map.save
+    redirect_to map_path(@map)
   end
   
   def destroy
@@ -14,6 +17,15 @@ class MapsController < ApplicationController
   end
   
   def show
-    
+    @map = Map.find(params[:id])
   end
+  
+  private
+    def map_params
+      params.require(:map).permit(:name)
+    end
+  
+    def moment_params
+      params.require(:map).permit(moments: [:memo, :latitude, :longitude])[:moments]
+    end
 end
