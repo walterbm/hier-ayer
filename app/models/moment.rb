af4e5aea8c25d0c4746d1efe7ceb80f0e0229871
walterbm <walter.beller.morales@gmail.com>
2015-08-04 14:15:32 -0400

    finish CRUD with Paperclip

--- app/models/moment.rb
+++ app/models/moment.rb
@@ -2,4 +2,8 @@ class Moment < ActiveRecord::Base
   belongs_to :map
   has_attached_file :image, styles: { small: "64x64", med: "100x100", large: "200x200" }
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
+
+  attr_accessor :delete_image
+  before_validation { image.clear if delete_image == '1'}
+
 end
