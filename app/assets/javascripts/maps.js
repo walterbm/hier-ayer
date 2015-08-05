f22c23ae00e6266a7226c7c3ce746d10763a0cc3
ryannyunn <ryannyunn@gmail.com>
2015-08-05 11:12:42 -0400

    added pins via moments to map, tada

--- app/assets/javascripts/maps.js
+++ app/assets/javascripts/maps.js
@@ -1,5 +1,4 @@
-
-function makeMap(latitude, longitude){
+function makeCurrentMap(latitude, longitude){
   L.mapbox.accessToken = 'pk.eyJ1Ijoid2FsdGVyYm0iLCJhIjoiMDU5ODljMDBjNzg3ZThlZTJlMTAwYWRhMTFjYWE0MzUifQ.CJ0ZCaTRHRMJTWDE0kIubA';
   var map = L.mapbox.map('map', 'mapbox.run-bike-hike')
    .setView([latitude, longitude], 16);
@@ -25,7 +24,32 @@ function makeMap(latitude, longitude){
         'marker-color': '#FF0066',
         'marker-symbol': 'danger'
     }
-}).addTo(map);
-
+  }).addTo(map);
 }
 
+function makeMomentMap(arr){
+  L.mapbox.accessToken = 'pk.eyJ1Ijoid2FsdGVyYm0iLCJhIjoiMDU5ODljMDBjNzg3ZThlZTJlMTAwYWRhMTFjYWE0MzUifQ.CJ0ZCaTRHRMJTWDE0kIubA';
+  var map = L.mapbox.map('map', 'mapbox.run-bike-hike');
+  var myLayer = L.mapbox.featureLayer().addTo(map);
+  var features = [];
+  
+  arr.forEach(function(marker){
+    features.push({
+      type: 'Feature',
+      geometry: {
+          type: 'Point',
+          coordinates: [parseFloat(marker[1]), parseFloat(marker[0])]
+      },
+      properties: {
+          'marker-color': '#000',
+          'marker-symbol': 'star-stroked',
+          title: [marker[0], marker[1]].join(',')
+      }
+    });
+  })
+  
+  myLayer.setGeoJSON({
+    type: 'FeatureCollection',
+    features: features
+  });
+}
