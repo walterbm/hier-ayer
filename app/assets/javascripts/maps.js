e60d7ad5b6ec24a0051a240729f5fc6151e9c4fd
walterbm <walter.beller.morales@gmail.com>
2015-08-05 18:16:36 -0400

    ajax the geojson request

--- app/assets/javascripts/maps.js
+++ app/assets/javascripts/maps.js
@@ -1,56 +1,23 @@
-function makeCurrentMap(latitude, longitude){
+function makeMomentMap(url){
   L.mapbox.accessToken = 'pk.eyJ1Ijoid2FsdGVyYm0iLCJhIjoiMDU5ODljMDBjNzg3ZThlZTJlMTAwYWRhMTFjYWE0MzUifQ.CJ0ZCaTRHRMJTWDE0kIubA';
-  var map = L.mapbox.map('map', 'mapbox.run-bike-hike')
-   .setView([latitude, longitude], 16);
-  L.mapbox.featureLayer({
-    // this feature is in the GeoJSON format: see geojson.org
-    // for the full specification
-    type: 'Feature',
-    geometry: {
-        type: 'Point',
-        // coordinates here are in longitude, latitude order because
-        // x, y is the standard for GeoJSON and many formats
-        coordinates: [
-          longitude, 
-          latitude
-        ]
-    },
-    properties: {
-        title: 'Current Location',
-        // one can customize markers by adding simplestyle properties
-        // https://www.mapbox.com/guides/an-open-platform/#simplestyle
-        'marker-size': 'large',
-        'marker-color': '#15b3d9',
-        'marker-symbol': 'star-stroked'
-    }
-  }).addTo(map);
-}
-
-function makeMomentMap(arr){
-  L.mapbox.accessToken = 'pk.eyJ1Ijoid2FsdGVyYm0iLCJhIjoiMDU5ODljMDBjNzg3ZThlZTJlMTAwYWRhMTFjYWE0MzUifQ.CJ0ZCaTRHRMJTWDE0kIubA';
-  var map = L.mapbox.map('map', 'mapbox.run-bike-hike')
-    .setView([parseFloat(arr[arr.length-1][0]), parseFloat(arr[arr.length-1][1])], 13);
+  var map = L.mapbox.map('map', 'mapbox.run-bike-hike');
   var myLayer = L.mapbox.featureLayer().addTo(map);
-  var features = [];
-  arr.forEach(function(marker){
-    features.push({
-      type: 'Feature',
-      geometry: {
-          type: 'Point',
-          coordinates: [parseFloat(marker[1]), parseFloat(marker[0])]
-      },
-      properties: {
-          'marker-color': '#15b3d9',
-          'marker-symbol': 'star-stroked',
-          description: marker[2]
-      }
-    });
-  })
-  
-  myLayer.setGeoJSON({
-    type: 'FeatureCollection',
-    features: features
-  });
+  $.get(url, function( data ) {
+    if (data.length === 0) {
+      getCoordinates(function(latitude,longitude){
+        map.setView([latitude,longitude],13);
+      });
+    }
+    else{
+      latitude = data[0].geometry.coordinates[1];
+      longitude = data[0].geometry.coordinates[0];
+      map.setView([latitude,longitude],13);
+      myLayer.setGeoJSON({
+        type: 'FeatureCollection',
+        features: data
+      });
+    } 
+  });  
   
   // leaflet bug hacked
   setTimeout(function () {
