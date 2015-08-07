5aa8a09bca66deabf4b876f953e3db4a1a64829c
ryannyunn <ryannyunn@gmail.com>
2015-08-06 21:25:40 -0400

    welcome page now shows all users maps and moments; does not show moment description or image

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,6 +1,32 @@
 class WelcomeController < ApplicationController
 
   def index
+    
+  end
+  
+  def geojson
+    @geojson = Array.new
+    @users = User.all
+    @users.each do |user|
+      user.maps.each do |map|
+        map.moments.each do |moment|
+          @geojson << {
+            type: 'Feature',
+            geometry: {
+              type: 'Point',
+              coordinates: [moment.longitude, moment.latitude]
+            },
+            properties: {
+              'marker-color': '#15b3d9',
+              'marker-symbol': 'star-stroked',
+              'marker-size': 'medium'
+            }
+          }
+        end
+      end
+    end
+
+     render json: @geojson
   end
   
 end
--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,8 @@ Rails.application.routes.draw do
     delete '/friends/:friend_id' => 'users#remove_friend'
   end
   
+  get '/geojson' => 'welcome#geojson'
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
