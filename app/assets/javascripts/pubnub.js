ecfbdcf68410da113cb89bdd8ad595347aef9a51
walterbm <walter.beller.morales@gmail.com>
2015-08-14 00:15:37 -0400

    enable SSL for pubnub requests

--- app/assets/javascripts/pubnub.js
+++ app/assets/javascripts/pubnub.js
@@ -1,5 +1,6 @@
 var PUBNUB_feed = PUBNUB.init({
-    subscribe_key: 'sub-c-c90f0d7c-422e-11e5-b72d-0619f8945a4f'
+    subscribe_key: 'sub-c-c90f0d7c-422e-11e5-b72d-0619f8945a4f',
+    ssl : true
   });
 
 PUBNUB_feed.subscribe({
